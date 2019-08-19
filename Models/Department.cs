928a06e3c8ecca7ecfcffbb9f1674c2d7442bd7b
Eliel Assis <eliel.assis7@hotmail.com>
2019-08-19 06:25:22 -0300

    Fisrt Model-Controller-view - Department

---
+++ Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Web.CodeGeneration.Utils;
+
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
