123f2ec9e2ce59bd540e748ded84da1006a24bbe
Eliel Assis <eliel.assis7@hotmail.com>
2019-08-23 09:50:15 -0300

    Foreign key not null (referencial integrity)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
